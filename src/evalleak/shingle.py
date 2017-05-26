bad8079d4e65cc87acd478bf389e0ab33d94c09d
roman1616 <32616350+roman1616@users.noreply.github.com>
2017-05-26 10:00:00 +0700

    refactor: containment rate

--- src/evalleak/shingle.py
+++ src/evalleak/shingle.py
@@ -7,3 +7,10 @@ how much text they share regardless of small edits.
 
 Computing exact Jaccard needs both full sets in memory and a set intersection
 per pair, which is quadratic in the number of records. A MinHash sketch trades
+a small, bounded error for a fixed-size signature: the fraction of matching
+minima across the signatures estimates the true Jaccard. The sketch here is
+built only on hashlib, so it is deterministic and depends on no third-party
+library.
+
+Determinism: the num_perm hash "permutations" are simulated by salting sha256
+with the permutation index. The same input always yields the same signature.
