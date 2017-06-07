d9619cc6728d8aa2b0a23d7bc71a7b7e8b956707
roman1616 <32616350+roman1616@users.noreply.github.com>
2017-06-07 13:48:00 +0700

    test: cover severity thresholds

--- src/evalleak/normalise.py
+++ src/evalleak/normalise.py
@@ -6,3 +6,11 @@ contamination. Making each step a separate flag keeps that trade off visible:
 you can see exactly which transformation caused two records to match.
 
 Steps, applied in this fixed order when enabled:
+
+1. whitespace: collapse every run of whitespace to a single space and strip
+   the ends. Line breaks and tabs become spaces.
+2. case: lowercase the text.
+3. punctuation: drop the ASCII punctuation characters, then re-collapse
+   whitespace so removed punctuation does not leave double spaces.
+
+The order matters. Case folding before punctuation removal does not change the
